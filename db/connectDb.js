f98efaa628ec7d3e3e6e653c9e7fc7195d1e3e85
Akashjitu18 <akashjitu18@gmail.com>
2024-09-12 00:04:55 +0530

    updated db connection

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,16 +1,30 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 
-const connectDb = async () => {
+// const connectDb = async () => {
+//     try {
+//       const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
+//         useNewUrlParser: true,
+//       });
+//       console.log(`MongoDB Connected: {conn.connection.host}`);
+//     } catch (error) {
+//       console.error(error.message);
+//       process.exit(1);
+//     }
+//   }
+
+//   export default connectDb
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
-        useNewUrlParser: true,
-      });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
+      const conn = await mongoose.connect(`${process.env.MONGODB_URI}/chai`);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
       console.error(error.message);
       process.exit(1);
     }
   }
 
-  export default connectDb
+  export default connectDB;
